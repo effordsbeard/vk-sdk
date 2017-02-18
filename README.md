e31f290f41ef862661243fb6b11e70dc39fda89f
Anton Petrov <le4dof@gmail.com>
2017-02-18 23:04:25 +0300

    no empty lines

--- README.md
+++ README.md
@@ -1,15 +1,20 @@
 # vk-sdk
 Python SDK for vk.com API
 
-### Wrapping
+### Wrapping  
 just for making requests to the API and not using models, 
 pass your parameters as strings or numbers to any method.
 
-`import vkapi`  
-`api = vkapi.API()`  
-`api.Newsfeed.search(q='query')`
-
+```python
+import vkapi  
+api = vkapi.API()  
+api.Newsfeed.search(q='query')
+```
+  
 or you can directly use subclasses
 
-`newsfeed = vkapi.Newsfeed()`  
-`newsfeed.search(q='query')`
+```python
+import vkapi
+newsfeed = vkapi.Newsfeed()  
+newsfeed.search(q='query')
+```
--- vkapi/newsfeed.py
+++ vkapi/newsfeed.py
@@ -8,92 +8,62 @@ class Newsfeed(VKAPI):
         super(Newsfeed, self).__init__(access_token=access_token)
 
     def add_ban(self, **params):
-
         self.set_method('addBan')
-
         return self.send(params)
 
     def delete_ban(self, **params):
-
         self.set_method('deleteBan')
-
         return self.send(params)
 
     def delete_list(self, **params):
-
         self.set_method('deleteList')
-
         return self.send(params)
 
     def get(self, **params):
-
         self.set_method('get')
-
         return self.send(params)
 
     def get_banned(self, **params):
-
         self.set_method('getBanned')
-
         return self.send(params)
 
     def get_comments(self, **params):
-
         self.set_method('getComments')
-
         return self.send(params)
 
     def get_lists(self, **params):
-
         self.set_method('getLists')
-
         return self.send(params)
 
     def get_mentions(self, **params):
-
         self.set_method('getMentions')
-
         return self.send(params)
 
     def get_recommended(self, **params):
-
         self.set_method('getRecommended')
-
         return self.send(params)
 
     def get_suggested_sources(self, **params):
-
         self.set_method('getSuggestedSources')
-
         return self.send(params)
 
     def ignore_item(self, **params):
-
         self.set_method('ignoreItem')
-
         return self.send(params)
 
     def save_list(self, **params):
-
         self.set_method('saveList')
-
         return self.send(params)
 
     def search(self, **params):
-
         self.set_method('search')
-
         return self.send(params)
 
     def unignore_item(self, **params):
-
         self.set_method('unignoreItem')
-
         return self.send(params)
 
     def unsubscribe(self, **params):
-
         self.set_method('unsubscribe')
-
         return self.send(params)
 
